2d98a4725bb89ffeb2e71846026a42bcb73114b3
Jose Sales <salesbass@gmail.com>
2016-11-01 10:36:12 -0200

    Inserindo usuarios com professor e aluno

--- src/main/resources/db/seed.sql
+++ src/main/resources/db/seed.sql
@@ -11,5 +11,9 @@
  INSERT INTO tb_user(  id, enable, image, name, password, username) VALUES (2, true, '', 'mr2', '$2a$10$teJrCEnsxNT49ZpXU7n22O27aCGbVYYe/RG6/XxdWPJbOLZubLIi2', 'mr2');
 
  --INSERT INTO role_user(role_id, user_id) values (2, 1);
- INSERT INTO user_role(role_id, user_id) values (1, 1);
- INSERT INTO user_role(role_id, user_id) values (2, 2);
+ INSERT INTO user_role(role_id, user_id) values (2, 1);
+ INSERT INTO user_role(role_id, user_id) values (1, 2);
+ 
+ INSERT INTO professor(id, id_usuario, nome) values (1, 1, 'mr');
+ INSERT INTO aluno(id, id_usuario, nome) values (1, 2, 'mr2');
+ 
